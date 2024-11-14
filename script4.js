bc1ac81c412172787b600bc005866425846acf72
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2024-11-14 15:34:57 -0500

    creo-script4

--- script4.js
+++ script4.js
@@ -1,1 +1,8 @@
-ejercicio2.html
+let numero = Math.floor(Math.random * 10 ) + 1;
+let adivina = prompt('Ingresa un número entre 1 y 10:');
+
+if(adivina === numero){
+    document.write('Felicitaciones, adivinaste el número...!');
+}else{
+    document.write('Lo siento, el número era: '+ numero);
+}
